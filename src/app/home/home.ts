229690c146ebcb4352a6c495eea7209e2a6b125b
Justin Becker <justin.becker1@gmx.de>
2025-07-19 11:24:40 +0200

    added title and meta

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
 
+export class Home implements OnInit{
+
+  constructor(private title: Title, private meta: Meta) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Justin Becker - Fullstack Web-Developer aus Wartenberg');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: 'Ich bin ein Fullstack-Webentwickler aus Hessen und liebe Angular, Django und allgemein kreatives Coding.'
+      },
+      {name: 'author', content: 'Justin Becker'},
+      {name: 'robots', 'content': 'index, follow'}
+    ])
+  }
 }
